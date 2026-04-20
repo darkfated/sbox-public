ceefb81df55afe9e3af3ff09b5d75c2a037adf60
darkfated <darkfated@icloud.com>
2026-04-20 13:56:44 +0300

    Refactor console row

--- game/addons/menu/Code/DevUI/Console/Console.cs
+++ game/addons/menu/Code/DevUI/Console/Console.cs
@@ -39,7 +39,7 @@ public class Console : Panel
 	{
 		Output = AddChild<VirtualList>();
 		Output.AddClass( "console_output" );
-		Output.ItemHeight = 20;
+		Output.ItemHeight = 22;
 		Output.OverscanCount = 2;
 		Output.PreferScrollToBottom = true;
 		Output.OnCreateCell = ( Panel cell, object data ) =>
